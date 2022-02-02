6e89458d3355f845f8e5de1e57a1e2b1eaaa646a
dhivatiradika <dhivatiradika@gmail.com>
2022-02-03 05:10:13 +0800

    add security improvement

--- core/src/main/java/com/dhiva/githubuser/core/di/CoreModule.kt
+++ core/src/main/java/com/dhiva/githubuser/core/di/CoreModule.kt
@@ -12,12 +12,16 @@ import com.dhiva.githubuser.core.data.source.remote.network.ApiService
 import com.dhiva.githubuser.core.domain.repository.IUserRepository
 import com.dhiva.githubuser.core.utils.AppExecutors
 import com.dhiva.githubuser.core.utils.SettingPreferences
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
@@ -26,24 +30,37 @@ val databaseModule = module {
     factory { get<UserDatabase>().userDao() }
     factory { get<UserDatabase>().userAttributesDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("githubuser".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             UserDatabase::class.java, "user_database"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
-        val loggingInterceptor =
-            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-        val client = OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
+        val hostname = "api.github.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/azE5Ew0LGsMgkYqiDpYay0olLAS8cxxNGUZ8OJU756k=")
+            .add(hostname, "sha256/vnCogm4QYze/Bc9r88xdA6NTQY74p4BAz2w5gxkLG2M=")
+            .add(hostname, "sha256/WoiWRyIOVNa9ihaBciRSC7XHjliYS9VwUGOIud4PB18=")
             .build()
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
+            .build()
+    }
+    single {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.github.com/")
             .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
+            .client(get())
             .build()
         retrofit.create(ApiService::class.java)
     }
